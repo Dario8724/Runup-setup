2b220e676caf8b907a09653a1bfa2063aa232743
Dário Bandanhe <dariobbandanhe@gmail.com>
2025-10-23 14:09:41 +0100

    Changed the HistoryDetailPage and started the inicial page layout

--- app/src/main/java/pt/iade/ei/runupsetup/InitialPageActivity.kt
+++ app/src/main/java/pt/iade/ei/runupsetup/InitialPageActivity.kt
@@ -1,24 +1,23 @@
 package pt.iade.ei.runupsetup
+
+import android.icu.util.Calendar
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.selection.selectable
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Face
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.LocationOn
@@ -26,11 +25,8 @@ import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.Label
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -45,42 +41,25 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import pt.iade.ei.runupsetup.models.HistoryItemModel1
 import pt.iade.ei.runupsetup.ui.theme.RunupSetupTheme
-import java.util.Calendar
 
-class HistoryDetailPage : ComponentActivity() {
+class InitialPageActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             RunupSetupTheme {
-                // criar a função DetailView e menciona-lo aqui
-                // criar uma página experimental para o projeto
-                //
-                HistoryDetailPageView()
+                InitialPageView()
             }
         }
     }
 }
-
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HistoryDetailPageView() {
-    val item = HistoryItemModel1(
-        title = "Corrida de Segunda",
-        date = Calendar.getInstance(),
-        distance = "5 km",
-        duration = "00:30:45",
-        calories = "250 kcal",
-        minimumPace = "5'30\"/km",
-        minimap = R.drawable.map_image
-    )
-
+fun InitialPageView() {
     Scaffold(
         topBar = {
             TopAppBar(
@@ -95,9 +74,9 @@ fun HistoryDetailPageView() {
                             onClick = {},
                             colors = ButtonDefaults.buttonColors(
                                 containerColor = Color.Black
-                            //  contentColor = Color.Unspecified
-                            // not necessary
-                              )
+                                //  contentColor = Color.Unspecified
+                                // not necessary
+                            )
 
                         ) {
                             Icon(
@@ -136,7 +115,7 @@ fun HistoryDetailPageView() {
                         ){
                             Icon(
                                 Icons.Default.Home,
-                                contentDescription ="Início",
+                                contentDescription ="Botão para a página inicial",
                                 tint = Color.Black,
 
                                 )
@@ -163,14 +142,14 @@ fun HistoryDetailPageView() {
                         ){
                             Icon(
                                 Icons.Default.LocationOn,
-                                contentDescription = "Rotas",
+                                contentDescription = "Botão para a página de rotas",
                                 tint = Color.Black
                             )
                             Text(
-                            text = "Rotas",
-                            fontSize = 10.sp,
-                            color = Color.Black
-                        )
+                                text = "Rotas",
+                                fontSize = 10.sp,
+                                color = Color.Black
+                            )
                         }
 
                         // added the location simbol since I can´t find the comunity icon
@@ -189,7 +168,7 @@ fun HistoryDetailPageView() {
                         ) {
                             Icon(
                                 Icons.Default.Person,
-                                contentDescription = "Comunidade",
+                                contentDescription = "Botão para a página de comunidade",
                                 tint = Color.Black
                             )
                             Text(
@@ -214,7 +193,7 @@ fun HistoryDetailPageView() {
                         ) {
                             Icon(
                                 Icons.Default.Info,
-                                contentDescription = "Histórico",
+                                contentDescription = "Botão para a página de histórico",
                                 tint = Color.Black
                             )
                             Text(
@@ -240,7 +219,7 @@ fun HistoryDetailPageView() {
                         ){
                             Icon(
                                 Icons.Default.AccountCircle,
-                                contentDescription = "Perfil",
+                                contentDescription = " Botão para a página de perfil",
                                 tint = Color.Black
                             )
                             Text(
@@ -257,92 +236,57 @@ fun HistoryDetailPageView() {
             }
         },
     ) { innerPadding ->
-
         Column(
             modifier = Modifier
-                .padding(innerPadding)
-            //.padding(30.dp)
+                .padding(innerPadding),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
         ) {
-            Row(
-                modifier = Modifier.padding(innerPadding),
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                Text(
-                    text = "As suas atividades recentes",
-                    fontFamily = FontFamily.SansSerif,
-                    fontWeight = FontWeight.Black,
-                    fontSize = 40.sp
+            Box{
+                Image(
+                    painter = painterResource(R.drawable.corredor_ao_por_do_sol),
+                    contentDescription = "Imagem de um corredor ao pôr do sol",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(250.dp)
                 )
-            }
-            Row (
-
-            ) {
-                Text(
-                    text = item.title,
-                    fontFamily = FontFamily.SansSerif,
-                    //fontWeight = FontWeight.Bold,
-                    fontSize = 30.sp
-                )
-            }
-            Spacer(modifier = Modifier.height(20.dp))
-            // Mapa da corrida com tamanho original
-            Image (
-                painter = painterResource(item.minimap),
-                contentDescription = "Mapa da corrida",
-                contentScale = ContentScale.FillBounds,
-                modifier = Modifier
-                    .height(250.dp)
-                    .width(width = 380.dp)
-            )
-            Spacer(
-                modifier = Modifier.height(30.dp)
-            )
-            Column {
-                Row(
-                    horizontalArrangement = Arrangement.SpaceEvenly,
-                    modifier = Modifier.fillMaxWidth()
+                Column(
+                    modifier = Modifier
+                        .align (Alignment.TopStart)
+                        .padding(start = 20.dp, top = 40.dp)
                 ) {
-                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                        Text(
-                            text = "Pace médio",
-                            fontWeight = FontWeight.Black
-                        )
-                        Text(text = item.minimumPace)
-                    }
-                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                        Text(
-                            text = "Tempo",
-                            fontWeight = FontWeight.Black
-                        )
-                        Text(text = item.duration)
-                    }
-                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                        Text(
-                            text = "Distância",
-                            fontWeight = FontWeight.Black
-                        )
-                        Text(
-                            text = item.distance
-                        )
-                    }
-                    Column(horizontalAlignment = Alignment.CenterHorizontally){
-                        Text(
-                            text = "Calorias",
-                            fontWeight = FontWeight.Black
-                        )
-                        Text(text = item.calories)
-                    }
+                    Text(
+                        text = "Olá, Corredor",
+                        fontWeight = FontWeight.Black,
+                        fontSize = 25.sp
+                    )
+                    Text(
+                        text = "Comece a se mover e alcance hoje mesmo suas metas de corrida!",
+                        fontWeight = FontWeight.Black,
+                        fontSize = 25.sp
+                    )
                 }
+            }
+            // Botão de iniciar
+            Button(
+                onClick = {},
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFF7CCE6B),
+                    contentColor = Color.Unspecified
+                )
+            ) {
+                Text(
+                    text = "Iniciar"
+                )
             }
         }
     }
 }
-// TODO: add labels below the icons and finish the history page
 
 @Preview(showBackground = true)
 @Composable
-fun HistoryDetailPagePreview() {
+fun InitialPagePreview() {
     RunupSetupTheme {
-        HistoryDetailPageView()
+       InitialPageView()
     }
 }
