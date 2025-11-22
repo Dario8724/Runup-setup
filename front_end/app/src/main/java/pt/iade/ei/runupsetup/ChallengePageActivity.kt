18c59597665c4dde4679bae86cc07d2b7c5b2654
EI-Zolana <erfrancisco268@gmail.com>
2025-11-22 13:27:48 +0000

    creatig functiosn for the  page

--- front_end/app/src/main/java/pt/iade/ei/runupsetup/ChallengePageActivity.kt
+++ front_end/app/src/main/java/pt/iade/ei/runupsetup/ChallengePageActivity.kt
@@ -3,6 +3,8 @@ package pt.iade.ei.runupsetup
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
 
 class ChallengePageActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,3 +18,25 @@ class ChallengePageActivity : ComponentActivity() {
 
 }
 
+data class ChallengeItem(
+    val title: String,
+    val participants: Int,
+    val progress: Int,
+    val daysLeft: Int,
+    val reward: String
+)
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ChallengePageView(){
+    val challenges = listOf(
+        ChallengeItem(
+            title = "Desafio 100km Outubro",
+            participants = 1243,
+            progress = 65,
+            daysLeft = 14,
+            reward = "Medalha Virtual"
+
+
+
+
+}
