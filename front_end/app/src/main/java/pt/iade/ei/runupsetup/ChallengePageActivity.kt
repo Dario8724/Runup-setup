dd3684e4a0787762ddbdcf715e31c9c02037a14c
EI-Zolana <erfrancisco268@gmail.com>
2025-11-24 23:21:31 +0000

    creating abas

--- front_end/app/src/main/java/pt/iade/ei/runupsetup/ChallengePageActivity.kt
+++ front_end/app/src/main/java/pt/iade/ei/runupsetup/ChallengePageActivity.kt
@@ -3,17 +3,28 @@ package pt.iade.ei.runupsetup
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import okhttp3.Challenge
@@ -67,7 +78,43 @@ fun ChallengePageView() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ChallengePage() {
-    Scaffold ( topBar = {  TopAppBar(title = {Text("Comunidade") },
+    Scaffold ( topBar = { TopAppBar(title = {Text("Comunidade") },
         colors = TopAppBarDefaults.topAppBarColors(containerColor = Color.White) )
 
 }
+    )
+
+    { innerPadding ->
+
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .padding(16.dp)
+        ) {
+
+            Text(
+                text = "Conecte-se com outros corredores",
+                color = Color.Gray
+            )
+
+            Spacer(modifier = Modifier.fillMaxWidth(20.dp) )
+
+
+                Row(modifier = Modifier.fillMaxWidth( ),
+                    horizontalArrangement = Arrangement.SpaceEvenly )
+
+            {
+                Box(modifier = Modifier
+                    .weight(1f)
+                    .padding(5.dp)
+                    .background(Color(0XFF7CCE6B),
+                        shape = RoundedCornerShape(12.dp)
+                )
+                    .padding(vertical = 12.dp), contentAlignment = Alignment.Center ) {
+                    Text("Feed", fontWeight = FontWeight.Bold)
+                }
+
+
+            }
+
+                )
